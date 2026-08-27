9d66681482a3b6c5063278605429cb4321d87c06
Murilo-SF <murilo.sf@github.com>
2026-08-27 17:12:43 -0300

    Adicionando markers aos testes

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -4,6 +4,8 @@ from Projeto_ORM.models.tabela_usuario import Usuario
 # =============================================================TESTE - ROTA "/auth/adicionar"==============================================================
 # ============================================================TESTE PARA REGISTRAR CORRETAMENTE============================================================
 
+
+@pytest.mark.auth
 def test_register_usuario(client):
     resposta = client.post("/auth/adicionar", json={"nome":"Nome Teste", "senha":"123456"})
     assert resposta.status_code == 201
@@ -12,6 +14,7 @@ def test_register_usuario(client):
 # ======================================================================TESTES ERRADOS======================================================================
 
 # Esse decorator serve para evitarmos repetição de testes e código, quando os testes tem a mesma regra de negócio, porém com dados diferentes
+@pytest.mark.auth
 @pytest.mark.parametrize(
     "dados, campo",
     [
@@ -26,11 +29,13 @@ def test_register_faltando_campo(client, campo, dados):
     assert resposta.get_json()["error"][campo][0] == ("Missing data for required field.")
 
 
+@pytest.mark.auth
 def test_register_faltando_dados(client):
     resposta = client.post("/auth/adicionar", json=None)
     assert resposta.status_code == 415
 
 
+@pytest.mark.auth
 def test_register_json_vazio(client):
     resposta = client.post("/auth/adicionar", json={})
     assert resposta.status_code == 400
@@ -38,6 +43,7 @@ def test_register_json_vazio(client):
     assert resposta.get_json()["error"] == {'nome': ['Missing data for required field.'], 'senha': ['Missing data for required field.']}
 
 
+@pytest.mark.auth
 @pytest.mark.parametrize(
         "dados, campo",
         [
@@ -54,6 +60,7 @@ def test_register_tipo_errado_dados(client, dados, campo):
     assert resposta.get_json()["error"][campo][0] == "Not a valid string."
 
 
+@pytest.mark.auth
 def test_register_chave_errada(client, admin_user):
     resposta = client.post("/auth/adicionar", json={"nome":admin_user.nome, "senha":"123456", "idade": 12})
     assert resposta.status_code == 400
@@ -61,6 +68,7 @@ def test_register_chave_errada(client, admin_user):
     assert resposta.get_json()["error"] == {'idade': ['Unknown field.']}
 
 
+@pytest.mark.auth
 def test_register_nome_duplicado(client, admin_user):
     resposta = client.post("/auth/adicionar", json={"nome":admin_user.nome, "senha":"123456"})
     assert resposta.status_code == 400
@@ -68,6 +76,7 @@ def test_register_nome_duplicado(client, admin_user):
     assert resposta.get_json()["error"] == "Usuário já existe!"
 
 
+@pytest.mark.auth
 def test_register_caracter_min_nome(client, admin_user):
     resposta = client.post("/auth/adicionar", json={"nome":"Mo", "senha":"123456"})
     assert resposta.status_code == 400
@@ -75,6 +84,7 @@ def test_register_caracter_min_nome(client, admin_user):
     assert resposta.get_json()["error"]["nome"][0] == "Shorter than minimum length 4."
 
 
+@pytest.mark.auth
 def test_register_caracter_min_senha(client, admin_user):
     resposta = client.post("/auth/adicionar", json={"nome":admin_user.nome, "senha":"123"})
     assert resposta.status_code == 400
@@ -84,6 +94,7 @@ def test_register_caracter_min_senha(client, admin_user):
 # ==============================================================TESTE - ROTA "/auth/login"==============================================================
 # ==================================================================TESTE LOGIN CORRETO=================================================================
 
+@pytest.mark.auth
 def test_login(client, admin_user):
     resposta = client.post("/auth/login", json={"nome":admin_user.nome,"senha":"123456"})
     assert resposta.status_code == 201
@@ -93,18 +104,21 @@ def test_login(client, admin_user):
 
 # ==================================================================TESTE LOGIN ERRADO==================================================================
 
+@pytest.mark.auth
 def test_login_usuario_nao_encontrado(client):
     resposta = client.post("/auth/login", json={"nome":"Nome", "senha":"123456"})
     assert resposta.status_code == 404
     assert "error" in resposta.get_json()
     assert resposta.get_json()["error"] == "Usuário não encontrado!"
 
+@pytest.mark.auth
 def test_login_senha_incorreta(client, admin_user):
     resposta = client.post("/auth/login", json={"nome":admin_user.nome, "senha":"654321"})
     assert resposta.status_code == 400
     assert "error" in resposta.get_json()
     assert resposta.get_json()["error"] == "SENHA INCORRETA!"
 
+@pytest.mark.auth
 @pytest.mark.parametrize(
         "dados, campo",
         [
@@ -117,19 +131,22 @@ def test_login_faltando_dados(client, dados, campo):
 
     assert resposta.status_code == 400
     assert "error" in resposta.get_json()
-    assert resposta.get_json()["error"][campo][0] == "Missing data for required sfield."
+    assert resposta.get_json()["error"][campo][0] == "Missing data for required field."
 
 
+@pytest.mark.auth
 def test_login_faltando_dados(client):
     resposta = client.post("/auth/login", json=None)
     assert resposta.status_code == 415
 
+@pytest.mark.auth
 def test_login_json_vazio(client):
     resposta = client.post("/auth/login", json={})
     assert resposta.status_code == 400
     assert "error" in resposta.get_json()
     assert resposta.get_json()["error"] == {"nome":['Missing data for required field.'], "senha":['Missing data for required field.']}
 
+@pytest.mark.auth
 @pytest.mark.parametrize(
         "dados, campos",
         [
@@ -145,18 +162,21 @@ def test_login_tipo_errado_dados(client, dados, campos):
     assert resposta.get_json()["error"][campos][0] == "Not a valid string."
 
 
+@pytest.mark.auth
 def test_login_chave_errada(client, admin_user):
     resposta = client.post("/auth/login", json={"nome":admin_user.nome, "senha":"123456", "idade":2})
     assert resposta.status_code == 400
     assert "error" in resposta.get_json()
     assert resposta.get_json()["error"]["idade"][0] == "Unknown field."
 
+@pytest.mark.auth
 def test_login_caracter_min_nome(client, admin_user):
     resposta = client.post("/auth/login", json={"nome":"Mo", "senha":"123456"})
     assert resposta.status_code == 400
     assert "error" in resposta.get_json()
     assert resposta.get_json()["error"]["nome"][0] == "Shorter than minimum length 4."
 
+@pytest.mark.auth
 def test_login_caracter_min_senha(client, admin_user):
     resposta = client.post("/auth/login", json={"nome":admin_user.nome, "senha":"123"})
     assert resposta.status_code == 400
@@ -165,6 +185,7 @@ def test_login_caracter_min_senha(client, admin_user):
 
 
 # Verificando durante os testes se o usuário realmente está no banco
+@pytest.mark.auth
 def test_usuario_admin_existe(admin_user, db_session):
     usuario_admin = db_session.get(Usuario, admin_user.id)
 
@@ -173,5 +194,6 @@ def test_usuario_admin_existe(admin_user, db_session):
     assert usuario_admin.role == "admin"
 
 
+@pytest.mark.auth
 def test_token_admin(token_admin): # token_admin retorna o token.
-    assert isinstance(token_admin, str)
+    assert isinstance(token_admin, str) # Confere se o a varíavel token admin é uma string
